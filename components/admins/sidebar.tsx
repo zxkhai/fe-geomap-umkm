dc3a6c869203d873c80aeade0b820d8d4ea40d01
dvlboo <kuhhhckr@gmail.com>
2025-11-08 08:13:20 +0700

    refactor : sidebar icons and logic active

--- components/admins/sidebar.tsx
+++ components/admins/sidebar.tsx
@@ -1,35 +1,37 @@
 import Link from "next/link";
 import { useRouter } from "next/router";
+import { FiShoppingBag } from "react-icons/fi";
+import { GoPerson } from "react-icons/go";
+import { MdLogout, MdOutlineDashboard } from "react-icons/md";
 
-const NavItem = ({ href, label }: { href: string; label: string }) => {
+const NavItem = ({ href, label, icon }: { href: string; label: string; icon: any }) => {
 const { pathname } = useRouter();
-const active = pathname === href;
+const active = pathname === href || pathname.startsWith(href + '/');
 
   return (
     <Link
-    href={href}
-    className={`flex items-center gap-3 px-5 py-3 rounded-xl transition 
+      href={href}
+      className={`flex items-center gap-3 px-5 py-3 rounded-xl transition 
       ${active ?"bg-black text-white":"text-gray-600 hover:bg-gray-200"}`}
-      >
-      <span className={`w-4 h-4 rounded ${active ? "bg-white" : "bg-gray-400"}`}/>
+    >
+      <span className={`w-5 h-5`}>{icon}</span>
       <span className="text-sm font-medium">{label}</span>
-      </Link>
+    </Link>
     );
 };
 
 export default function Sidebar() {
   return (
-    // sidebar sits below the topbar; make it sticky and scrollable independently
     <aside className="hidden md:block w-52 bg-gray-100 sticky top-20 h-[calc(100vh-5rem)] overflow-auto">
       <nav className="px-3 space-y-2 mt-4">
-        <NavItem href="/admin/dashboard" label="Dashboard" />
-        <NavItem href="/admin/umkm" label="Data UMKM" />
-        <NavItem href="/admin/profile" label="Profile" />
+        <NavItem href="/admin/dashboard" label="Dashboard" icon={<MdOutlineDashboard />}/>
+        <NavItem href="/admin/umkm" label="Data UMKM" icon={<FiShoppingBag />}/>
+        <NavItem href="/admin/profile" label="Profile" icon={<GoPerson />}/>
       </nav>
 
       <div className="absolute left-0 bottom-0 w-full p-4">
-        <button className="w-full flex items-center justify-center gap-2 bg-black text-white py-2 rounded-xl">
-          Log out <span>↗</span>
+        <button className="w-full flex items-center justify-center gap-2 bg-black text-white py-2 rounded-xl hover:cursor-pointer hover:bg-gray-950">
+          Log out <span><MdLogout /></span>
         </button>
       </div>
     </aside>
